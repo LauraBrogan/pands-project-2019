1c043449f9996d2bff259b45c73f70df0c27af82
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-04-19 09:17:17 +0100

    Trying to read csv file and print to screen

--- Iris.py
+++ Iris.py
@@ -1,18 +1,19 @@
 import pandas as pd
-from pandas import DataFrame
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib
-import seaborn as sns
+#from pandas import DataFrame
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import matplotlib
+#import seaborn as sns
 
-from pandas import DataFrame
-from matplotlib import style
-sns.set(color_codes=True)
+#from pandas import DataFrame
+#from matplotlib import style
+#sns.set(color_codes=True)
 
 #%matplotlib inline
 #%pylab inline
 
 data = pd.read_csv ("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/d546eaee765268bf2f487608c537c05e22e4b221/iris.csv")
-data_top = data.head() 
-data_top 
+#data_top = data.head() 
+#data_top 
+print(data)
 

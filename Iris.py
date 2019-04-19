d5e355b1dc4f9c8db60d1d15e7aa84e42b586cf5
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-04-19 09:32:24 +0100

    Iris.py First research into data set to return first 10 lines of the data set.

--- Iris.py
+++ Iris.py
@@ -1,19 +1,18 @@
+# Initial Python Code to Just get the first ten lines of the data set returned to screen.
+# To start I import pandas as pd
+
 import pandas as pd
-#from pandas import DataFrame
-#import numpy as np
-#import matplotlib.pyplot as plt
-#import matplotlib
-#import seaborn as sns
 
-#from pandas import DataFrame
-#from matplotlib import style
-#sns.set(color_codes=True)
-
-#%matplotlib inline
-#%pylab inline
-
+#Identify data as pandas reading the csv file.
 data = pd.read_csv ("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/d546eaee765268bf2f487608c537c05e22e4b221/iris.csv")
-#data_top = data.head() 
-#data_top 
-print(data)
 
+#Identify Data To as the data head for 10 line.
+data_top = data.head(10) 
+
+#Then I desplay the data top to the user. 
+
+print(data_top) 
+
+#Reference:https://www.geeksforgeeks.org/python-pandas-dataframe-series-head-method/
+
+# Laura Brogan 19/04/2019

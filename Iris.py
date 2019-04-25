9345e387d40474c0b8982cfb9f3315729d8cfaab
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-04-25 20:54:46 +0100

    Completed script iris.py 25/04/2019

--- Iris.py
+++ Iris.py
@@ -6,12 +6,16 @@ import pandas as pd
 #Identify data as pandas reading the csv file.
 data = pd.read_csv ("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/d546eaee765268bf2f487608c537c05e22e4b221/iris.csv")
 
-#Identify Data To as the data head for 10 line.
+#Identify Data Top as the data head for the first 10 lines of the data set.
 data_top = data.head(10) 
 
-#Then I desplay the data top to the user. 
+#Identify data bottom as the last 10 lines of the data set. 
+data_bottom = data.tail(10)
+
+#Then I display the data top and data bottom to the user. 
 print(data_top) 
+print(data_bottom)
 
 #Reference:https://www.geeksforgeeks.org/python-pandas-dataframe-series-head-method/
 
-#Laura Brogan 19/04/2019
+#Laura Brogan 25/04/2019

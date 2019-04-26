145f9db0b4fbafb7b8069d323beb415920641847
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-04-26 19:51:59 +0100

    Complete boxpl.py 26/04/2019

--- boxpl.py
+++ boxpl.py
@@ -1,4 +1,4 @@
-#This will display to the user a box diagram of the diamentions of the sepal length for each of the three iris species.
+#This will display to the user a box diagram of the dimentions of the sepal length for each of the three iris species.
 #Import pandas program as pd.
 import pandas as pd
 
@@ -11,10 +11,10 @@ import matplotlib.pyplot as plt
 #This is setting the structure of the box plot. 
 sns.set(style="whitegrid", palette="GnBu_d", rc={'figure.figsize':(11.7,8.27)})
 
-#Iris_dats is reading in the data set. 
+#Iris_dats is reading in the data set csv file. 
 iris_data = pd.read_csv ("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/d546eaee765268bf2f487608c537c05e22e4b221/iris.csv")
 
-#Label the box chart the the title Comparing the Distribution of Sepal Length.
+#Label the box chart with the title Comparing the Distributions of Sepal Length.
 title="Comparing the Distributions of Sepal Length"
 
 #Box plot x axis is the Species, y axis is the Sepal Length and the data is from irsi_data.

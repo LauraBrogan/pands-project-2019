d9736c7e8e8e0bdf324959c931b773c69cf868b9
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-04-26 19:39:00 +0100

    Complete irissum.py 26/04/2019

--- irissum.py
+++ irissum.py
@@ -1,14 +1,14 @@
-# This is to obtain a summary of the data contained in the iris data set, the .
+# This is to obtain a summary of the data contained in the iris data set.
 # To start I import pandas as pd
 import pandas as pd
 
 #Identify data as iris_data reading the csv file.
 iris_data = pd.read_csv ("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/d546eaee765268bf2f487608c537c05e22e4b221/iris.csv")
 
-#This gives descriptive statistics that summarise all samples take for the four categories, septal lenght, septal width, petal length, petal wedth
+#This gives descriptive statistics that summarises all samples takean for the four categories, septal length, septal width, petal length, petal width
 summary = iris_data.describe()
 
-#The datat is transposed into a table format for the user. 
+#The data is transposed into a table format for the user. 
 summary = summary.transpose()
 
 #Then I dissplay the summary data to the user. 
